1179b477c0674f31e2a22d07ccaa873c4000ed53
mahsin <mahsin1912@gmail.com>
2024-01-18 13:18:14 +0600

    Hompage adding MeningFull content

--- src/Component/HomeComponents/Features/Features.js
+++ src/Component/HomeComponents/Features/Features.js
@@ -1,20 +1,36 @@
-"use client"
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
 import { GiVote } from "react-icons/gi";
-import { IoCreateOutline } from "react-icons/io5";
-import { MdHowToVote } from "react-icons/md";
+import { MdAddBox } from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
 
 const Features = () => {
-    return (
-        <div className='my-10'>
-            <h1>Features</h1>
-            <div className='grid lg:grid-cols-3'>
-                <div>Create election Category <IoCreateOutline className='text-[50px]' /> </div>
-                <div>All Voting right now <GiVote className='text-[50px]' /></div>
-                <div>All result <MdHowToVote className='text-[50px]' /></div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="my-16">
+      <h1 className="text-3xl lg:text-5xl font-bold lg:font-semibold mb-7 lg:pl-0 pl-9">Access Our Services</h1>
+      <div className="grid lg:grid-cols-3 gap-8">
+        <Link href="/">
+          <div className="flex flex-col justify-center items-center bg-white text-[#00263A] py-12 lg:rounded-xl">
+            <p className="text-3xl font-bold mb-3">Organize voting</p>
+            <MdAddBox className="text-[50px]" />
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="flex flex-col justify-center items-center bg-white text-[#00263A] py-12 lg:rounded-xl">
+            <p className="text-3xl font-bold mb-[10px]">Live voting</p>
+            <GiVote className="text-[55px]" />
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="flex flex-col justify-center items-center bg-white text-[#00263A] py-12 lg:rounded-xl">
+            <p className="text-3xl font-bold mb-4">Voting histories</p>
+            <FaHistory className="text-[45px]" />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
