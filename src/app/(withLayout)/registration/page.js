b91ae1dbd3a0456a8c565a07566ce22f82846d65
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-01-24 09:38:20 +0600

    resistration page completed creted by junyet

--- src/app/(withLayout)/registration/page.js
+++ src/app/(withLayout)/registration/page.js
@@ -1,22 +1,47 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Registration = () => {
-  const handleRegistration = (event) => {
-    event.preventDefault();
-    const from = event.target;
+  const[Error, seterror] = useState("")
+  const[loading, setloading]=useState(false)
+  const handleRegistration = async(e) => {
+    e.preventDefault();
+    const from = e.target;
     const name = from.name.value;
     const email = from.email.value;
     const password = from.password.value;
     const photo = from.photo.value;
-    console.log(name, email, password, photo);
+    const date = from.date.value;
+    const idNumber =from.idNumber.value;
+    const user ={name, email, password, photo,date,idNumber}
+    console.log(user)
+    try{
+      setloading(true)
+      const res = await fetch("http://localhost:3000/api/signup",{
+          method:"POST",
+          headers:{
+           "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+         });
+         if(res.ok){
+          seterror(await res.json())
+           from.reset()
+        //  router.push('/login')
+         }
+     }catch(err){
+      console.log(err)
+      setloading(false)
+     }finally{
+      setloading(false)
+     }
   };
   return (
   
     <div>
-      <div className="shadow-2xl bg-base-200 rounded-lg">
+      <div className="my-8 p-6 bg-gray-800 rounded-md shadow-md">
         <div className="flex justify-center mx-auto">
           <Image
             src="https://i.postimg.cc/s2Nhzyd5/signup-removebg-preview.png"
@@ -30,68 +55,101 @@ const Registration = () => {
           <div className="hero-content  ">
             <div className=" w-full   ">
               <form onSubmit={handleRegistration} className="card-body">
-                <div className="grid lg:grid-cols-2 gap-2">
+                <div className="grid lg:grid-cols-3 gap-4">
                   <div>
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text">Name</span>
+                        <span className=" text-white">Name</span>
                       </label>
                       <input
                         type="text"
                         placeholder="Enter Your Name"
-                        className="border-2 p-2 rounded-sm"
+                        className="w-full px-4 py-2  bg-gray-700 text-white rounded-md focus:outline-none focus:border-indigo-500"
                         required
                         name="name"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text">E-mail</span>
+                        <span className=" text-white">ID Card Number</span>
                       </label>
                       <input
                         type="text"
-                        placeholder="Enter Your E-mail"
-                        className="border-2 p-2 rounded-sm"
+                        placeholder="Photo"
+                        className="w-full px-4 py-2  bg-gray-700 text-white rounded-md focus:outline-none focus:border-indigo-500"
                         required
-                        name="email"
+                        name="idNumber"
                       />
                     </div>
                   </div>
                   <div>
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text">Password</span>
+                        <span className=" text-white">Password</span>
                       </label>
                       <input
                         type="password"
                         placeholder="Password"
-                        className="border-2 p-2 rounded-sm"
+                        className="w-full px-4 py-2  bg-gray-700 text-white rounded-md focus:outline-none focus:border-indigo-500"
                         required
                         name="password"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text">Upload Picture</span>
+                        <span className=" text-white">Upload Picture</span>
                       </label>
                       <input
                         type="text"
                         placeholder="Photo"
-                        className="border-2 p-2 rounded-sm"
+                        className="w-full px-4 py-2  bg-gray-700 text-white rounded-md focus:outline-none focus:border-indigo-500"
                         required
                         name="photo"
                       />
                     </div>
-                  </div>
+                    </div>
+                   <div>
+                   <div className="form-control">
+                      <label className="label">
+                        <span className=" text-white">E-mail</span>
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter Your E-mail"
+                        className="w-full px-4 py-2  bg-gray-700 text-white rounded-md focus:outline-none focus:border-indigo-500"
+                        required
+                        name="email"
+                      />
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className=" text-white">Birth of Date</span>
+                      </label>
+                      <input
+                        type="date"
+                        placeholder="Photo"
+                        className="w-full px-4 py-2  bg-gray-700 text-white rounded-md focus:outline-none focus:border-indigo-500"
+                        required
+                        name="date"
+                      />
+                    </div>
+                   </div>
+                  
                 </div>
-                <div className="form-control mt-6 w-full ">
-                  <button className="p-2  button text-white bg-gray-500 shadow-2xl hover:bg-slate-400 rounded-sm">
-                    Sign Up
+                {
+                  Error && <p className="">{Error.message}</p>
+                  }
+                
+                <div className="form-control mt-3 w-full ">
+                  <button disabled={loading} className="p-2  button text-white bg-gray-500 shadow-2xl hover:bg-slate-400 rounded-sm">
+                    {
+                      loading?"loading....":"Sign Up"
+                    }
                   </button>
                 </div>
                 <label className="label">
                   Already have an account?{" "}
-                  <span className="text-[#205c67] ">
+                  <span className="text-white ">
                     {" "}
                     <Link href="/login">Log In</Link>
                   </span>
