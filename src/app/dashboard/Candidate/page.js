1f02a1ca568e5a8dadbfa3b6096ea8e8e5ef3245
chandonkumar23 <chandonkumar070@gmail.com>
2024-01-26 12:22:31 +0600

    show all candidate and voter on the UI

--- src/app/dashboard/Candidate/page.js
+++ src/app/dashboard/Candidate/page.js
@@ -1,70 +1,24 @@
-import Image from "next/image";
-const page = async () => {
-  const res = await fetch("https://evs-delta.vercel.app/candidate");
-  const candidates = await res.json();
-  console.log(candidates);
+"use client";
+import { useEffect, useState } from "react";
+import CandidateCart from "./CandidateCart";
+
+const Page = () => {
+  const [candidates, setCandidates] = useState([]);
+
+  useEffect(() => {
+    fetch("https://evs-delta.vercel.app/candidate")
+      .then((res) => res.json())
+      .then((data) => setCandidates(data));
+  }, []);
+
   return (
     <div>
-      <p>All Candidates: {candidates.length}</p>
-      {candidates.map((candidate) => (
-        <div key={candidate.id}>
-          <div className="overflow-x-auto">
-            <table className="table">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <th>Candidate</th>
-                  <th>ID Card NUmber</th>
-                  <th>Brand</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* row 1 */}
-                <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                          <Image
-                            src={candidate.candidatePhoto}
-                            width={500}
-                            height={500}
-                            alt="candidate image"
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <div className="font-bold">
-                          {candidate.candidateName}
-                        </div>
-                        
-                      </div>
-                    </div>
-                  </td>
-                  <td>{candidate.candidateID}</td>
-                  <td>{candidate.brand}</td>
-                  <th>
-                    <button className="btn btn-ghost btn-xs">Information</button>
-                  </th>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+     {candidates?.map((candidate) => (
+        <CandidateCart key={candidate._id} candidate={candidate}></CandidateCart>
       ))}
+       
     </div>
   );
 };
 
-export default page;
+export default Page;
--- src/app/dashboard/allVoter/page.js
+++ src/app/dashboard/allVoter/page.js
@@ -1,150 +1,25 @@
-const page = () => {
+"use client";
+import { useEffect, useState } from "react";
+import AllVoter from "./AllVoter";
+
+
+const Page = () => {
+  const [voters, setVoters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://evs-delta.vercel.app/users")
+      .then((res) => res.json())
+      .then((data) => setVoters(data));
+  }, []);
+
   return (
     <div>
-        <h2>All Voter</h2>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>Voter Name</th>
-              <th>ID Card Number</th>
-              <th>E-mail</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12"></div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12"></div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Tax Accountant
-                </span>
-              </td>
-              <td>Red</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12"></div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Rowe-Schoen
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Office Assistant I
-                </span>
-              </td>
-              <td>Crimson</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 4 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12"></div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Wyman-Ledner
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Community Outreach Specialist
-                </span>
-              </td>
-              <td>Indigo</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-          </tbody>
-         
-         
-        </table>
-      </div>
+     {voters?.map((voter) => (
+        <AllVoter key={voter._id} voter={voter}></AllVoter>
+      ))}
+       
     </div>
   );
 };
-export default page;
+
+export default Page;
